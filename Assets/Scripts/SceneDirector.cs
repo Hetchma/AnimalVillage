967131e5b8ca1cbca6ad74856e816659270c6efc
KazukiShimura <73980807+KazukiShimura@users.noreply.github.com>
2021-04-17 20:31:16 +0900

    Initial commit

---
+++ Assets/Scripts/SceneDirector.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class SceneDirector : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        var button = GetComponent<Button>();
+
+        button.onClick.AddListener(() => SceneManager.LoadScene("MainScene"));
+    }
+}
---
+++ Assets/Scripts/SettingManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class SettingManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OpenSetting()
+    {
+        gameObject.SetActive(true);
+    }
+}
