669fd84d452ced5cc3e590fc8494e85590d17560
KazukiShimura <73980807+KazukiShimura@users.noreply.github.com>
2021-04-21 20:33:25 +0900

    add StartCameraWork
    起動時のカメラ回転エフェクト追加

--- Assets/Scripts/StartPanel.cs
+++ Assets/Scripts/StartPanel.cs
@@ -1,13 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class StartPanel : MonoBehaviour
 {
+    public GameObject targetObject;
+    public GameObject mainCamera;
+
     void Update()
     {
+        mainCamera.transform.RotateAround(targetObject.transform.position, new Vector3(0, 1, 0), -0.3f);
+
+
         if (EventSystem.current.IsPointerOverGameObject())
         {
             return;
@@ -17,5 +20,7 @@ public class StartPanel : MonoBehaviour
     public void StartButton()
     {
         Destroy(gameObject);
+        mainCamera.transform.position = new Vector3(0, 10f, -20f);
+        mainCamera.transform.rotation = Quaternion.Euler(25, 0, 0);
     }
 }
