e1a88e84f6a6f7d873c1a005f5b1a8ad3e867f53
Hetchma <hetchma.game@gmail.com>
2021-04-22 00:09:43 +0900

    update title
    タイトルTMP更新

--- Assets/Scripts/StartPanel.cs
+++ Assets/Scripts/StartPanel.cs
@@ -5,10 +5,11 @@ public class StartPanel : MonoBehaviour
 {
     public GameObject targetObject;
     public GameObject mainCamera;
+    public GameObject UICanvas;
 
     void Update()
     {
-        mainCamera.transform.RotateAround(targetObject.transform.position, new Vector3(0, 1, 0), -0.3f);
+        mainCamera.transform.RotateAround(targetObject.transform.position, new Vector3(0, 1, 0), -0.1f);
 
 
         if (EventSystem.current.IsPointerOverGameObject())
@@ -20,6 +21,7 @@ public class StartPanel : MonoBehaviour
     public void StartButton()
     {
         Destroy(gameObject);
+        UICanvas.SetActive(true);
         mainCamera.transform.position = new Vector3(0, 10f, -20f);
         mainCamera.transform.rotation = Quaternion.Euler(25, 0, 0);
     }
