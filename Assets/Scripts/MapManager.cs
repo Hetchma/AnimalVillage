4c0e1519d0d678004efb5113a3256297fdb9b7ef
KazukiShimura <73980807+KazukiShimura@users.noreply.github.com>
2021-04-22 20:22:46 +0900

    update newmapbutton
    マップ生成ボタン使用後の無効化設定追加

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -1,5 +1,6 @@
 ﻿using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MapManager : MonoBehaviour
 {
@@ -8,28 +9,37 @@ public class MapManager : MonoBehaviour
     public GameObject Penguin_Map;
     public GameObject Lion_Map;
     public GameObject PlusMapPanel;
+    public Button Dog_Map_B;
+    public Button Chicken_Map_B;
+    public Button Penguin_Map_B;
+    public Button Lion_Map_B;
+
 
     public void Dog_Map_Button()
     {
         Dog_Map.transform.DOMove(new Vector3(0, 0, 0), 2f).SetEase(Ease.OutElastic);
         PlusMapPanel.SetActive(false);
+        Dog_Map_B.interactable = false;
     }
 
     public void Chicken_Map_Button()
     {
         Chicken_Map.transform.DOMove(new Vector3(0, 0, 0), 2f).SetEase(Ease.OutElastic);
         PlusMapPanel.SetActive(false);
+        Chicken_Map_B.interactable = false;
     }
 
     public void Penguin_Map_Button()
     {
         Penguin_Map.transform.DOMove(new Vector3(0, 0, 0), 2f).SetEase(Ease.OutElastic);
         PlusMapPanel.SetActive(false);
+        Penguin_Map_B.interactable = false;
     }
 
     public void Lion_Map_Button()
     {
         Lion_Map.transform.DOMove(new Vector3(0, 0, 0), 2f).SetEase(Ease.OutElastic);
         PlusMapPanel.SetActive(false);
+        Lion_Map_B.interactable = false;
     }
 }
