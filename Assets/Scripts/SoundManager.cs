e06715aad49b9ca51385ff60054a119c952fc514
Hetchma <hetchma.game@gmail.com>
2021-04-29 10:23:42 +0900

    add BGM
    BGM追加

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -3,17 +3,25 @@
 public class SoundManager : MonoBehaviour
 {
     [SerializeField] GameObject OnSoundButton;
+    AudioSource[] audioSources;
+
+    private void Start()
+    {
+        audioSources = GetComponents<AudioSource>();
+    }
 
     public void OnSound()
     {
         OnSoundButton.SetActive(false);
+        audioSources[0].mute = true;
+        audioSources[1].mute = true;
     }
 
     public void OffSound()
     {
         OnSoundButton.SetActive(true);
+        audioSources[0].mute = false;
+        audioSources[1].mute = false;
     }
-
-
 
 }
