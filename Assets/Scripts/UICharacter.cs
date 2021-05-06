b79dc43700457b1369ea78fb230ea31cbbbb0a07
KazukiShimura <73980807+KazukiShimura@users.noreply.github.com>
2021-05-06 20:16:05 +0900

    add UICharacterMotion
    動物選択時のモーション追加

---
+++ Assets/Scripts/UICharacter.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class UICharacter : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0.8f, 0));
+    }
+}
