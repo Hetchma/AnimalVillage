a004fa1f3f3e6b33879cbb63d1405be87c06cce5
KazukiShimura <73980807+KazukiShimura@users.noreply.github.com>
2021-05-08 19:54:36 +0900

    add QuitButton
    アプリケーション終了ボタン追加

--- Assets/Scripts/SettingManager.cs
+++ Assets/Scripts/SettingManager.cs
@@ -2,12 +2,19 @@
 
 public class SettingManager : MonoBehaviour
 {
-
-
-
-
     public void OnSettingPanel()
     {
         gameObject.SetActive(true);
     }
+
+    //ゲーム終了ボタン処理
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_STANDALONE
+      UnityEngine.Application.Quit();
+#endif
+    }
+
 }
